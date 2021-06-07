8ada1ad8bdc7b33458b49fd553f5fbbf8396f372
Harkarman Singh <harkarman.web@gmail.com>
2021-06-07 19:55:15 +0530

    create basic functionality TODO: Improve UI using TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { format, fromUnixTime } from "date-fns";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [url, setUrl] = useState([
+    "http://www.mocky.io/v2/59b3f0b0100000e30b236b7e",
+  ]);
+  // const urls = [
+  //   "http://www.mocky.io/v2/59b3f0b0100000e30b236b7e",
+  //   "http://www.mocky.io/v2/59ac28a9100000ce0bf9c236",
+  //   "http://www.mocky.io/v2/59ac293b100000d60bf9c239",
+  // ];
+
+  const sortArray = (type) => {
+    const types = {
+      likes: "likes",
+      views: "views",
+      shares: "shares",
+      event_date: "event_date",
+    };
+    const sortProperty = types[type];
+    const sorted = [...posts].sort((a, b) => b[sortProperty] - a[sortProperty]);
+    setPosts(sorted);
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data.posts);
+      })
+      .catch((err) => console.log(err));
+  }, [url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <button
+          onClick={() => {
+            setUrl("http://www.mocky.io/v2/59b3f0b0100000e30b236b7e");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          1
+        </button>
+        <button
+          onClick={() => {
+            setUrl("http://www.mocky.io/v2/59ac28a9100000ce0bf9c236");
+          }}
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            setUrl("http://www.mocky.io/v2/59ac293b100000d60bf9c239");
+          }}
+        >
+          3
+        </button>
+      </div>
+      <div>
+        <select onChange={(e) => sortArray(e.target.value)}>
+          <option value="likes">Likes</option>
+          <option value="views">Views</option>
+          <option value="shares">Shares</option>
+          <option value="event_date">Date</option>
+        </select>
+      </div>
+      <div>
+        {posts.length > 1 ? (
+          <div>
+            {posts.map((post) => (
+              <div
+                key={post.thumbnail_image}
+                style={{
+                  display: "flex",
+                  padding: 10,
+                  justifyContent: "space-evenly",
+                }}
+              >
+                <p>{post.id}</p>
+                <img
+                  src={post.thumbnail_image}
+                  alt={post.id}
+                  style={{ width: 150 }}
+                />
+                <p>{post.event_name}</p>
+                <p>Views: {post.views}</p>
+                <p>Likes: {post.likes}</p>
+                <p>Shares: {post.shares}</p>
+                <p>{format(fromUnixTime(post.event_date), "MM/dd/yyyy")}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>No posts found</div>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
